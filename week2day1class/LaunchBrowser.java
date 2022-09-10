c52c33d38fed80ac3b35000cca4a531fcadd5ff4
shyamalajeeva <112800801+shyamalajeeva@users.noreply.github.com>
2022-09-10 13:33:24 -0700

    Add files via upload

---
+++ week2day1class/LaunchBrowser.java
@@ -0,0 +1,69 @@
+package week2day1class;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver driver=new EdgeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		//find username
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		//find password
+		 driver.findElement(By.id("password")).sendKeys("crmsfa");
+	
+	 //click the button
+    driver.findElement(By.className("decorativeSubmit")).click();
+    //GetText
+    String text = driver.findElement(By.tagName("h2")).getText();
+    //print the tagname
+    System.out.println(text);
+    //click crm/sfa
+    driver.findElement(By.linkText("CRM/SFA")).click();
+    //click leadsbutton;
+    driver.findElement(By.linkText("Leads")).click();
+    //click createlead;
+    driver.findElement(By.linkText("Create Lead")).click();
+    //click enetr companyname 
+    driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf1");
+    //firstname
+    driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Shyamala");
+    //lastname
+    driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Jeeva");
+    //firstname local
+    driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Shyama");
+    //department
+   //dep
+	 //
+	  driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Digital testing");
+
+    
+    //description field
+  //description
+  	 //createLeadForm_description
+	  driver.findElement(By.id("createLeadForm_description")).sendKeys("working on first time.success");
+	  	
+    //email //email-
+
+  	  driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("shyamalajeeva17@gmail.com");
+  	  
+  	  
+  	  //create lead-smallSubmit //create button
+  	 driver.findElement(By.className("smallSubmit")).click();
+  	 
+  	String title2 = driver.getTitle();
+	
+	System.out.println("newpage"+title2);
+	
+  	 
+  	
+    
+	}}
